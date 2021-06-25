924d453756c5cefe1ceb39d137346f670e11dfed
chen hong <vvchennnnn@gmail.com>
2021-06-25 12:29:20 +1000

    part1 config change

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,9 @@ module.exports = {
                     presets: ['@babel/preset-react']
                 }
             }
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
         }]
     }
 }
