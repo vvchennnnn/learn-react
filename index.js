44f05178c5ec67a1c1b989d29a13aef9a93ebb7f
chen hong <vvchennnnn@gmail.com>
2021-06-28 12:43:01 +1000

    part2 index modified

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
 
+// 统一管理全局 maintainable
+import './main.css';
+
 // ReactDom.render(
 //     // tag name, attribute, children
 //     React.createElement('div', {
